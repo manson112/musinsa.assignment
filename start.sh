30fad90e1b4a37e383d85bfe635d9ff545a6a3e0
manson112 <rlarhksdn777@naver.com>
2022-10-01 01:28:35 +0900

    gradlew 수행 명령어 수정

--- start.sh
+++ start.sh
@@ -18,10 +18,10 @@ if [[ "$_java" ]]; then
     version=$("$_java" -version 2>&1  | head -1 | cut -d'"' -f2 | sed 's/^1\.//' | cut -d'.' -f1)
     echo version "$version"
     if [[ "$version" -eq "11" ]]; then
-        ./gradlew clean build bootrun
+        ./gradlew clean bootrun
     else
         sudo yum install -y java-11-amazon-corretto.x86_64
         java11=$(alternatives --list | grep java-11 | cut -f3)
-        ./gradlew clean build -Dorg.gradle.java.home="$java11" bootrun
+        ./gradlew clean bootrun -Dorg.gradle.java.home="$java11"
     fi
 fi
